8dfc7c18069f3f7e4bc857b162fb5cfb79677627
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2025-05-19 01:04:29 -0500

    Corregido: agregada configuración de JaCoCo
    *   Asegurar que la compilación falle si la cobertura de código es inferior al 90%.
    *   se agregaron comentarios para el java doc

--- productos/src/main/java/com/pragma/ProductosApplication.java
+++ productos/src/main/java/com/pragma/ProductosApplication.java
@@ -3,11 +3,24 @@ package com.pragma.productos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal para la aplicación de microservicio de Productos.
+ * Esta clase es el punto de entrada para la aplicación Spring Boot.
+ * <p>
+ * La anotación {@code @SpringBootApplication} habilita la autoconfiguración de Spring Boot,
+ * el escaneo de componentes y la configuración específica de Spring Boot.
+ * </p>
+ *
+ */
 @SpringBootApplication
 public class ProductosApplication {
 
+    /**
+     * Método principal que sirve como punto de entrada para la aplicación Spring Boot.
+     *
+     * @param args Argumentos de línea de comandos pasados al iniciar la aplicación.
+     */
     public static void main(String[] args) {
         SpringApplication.run(ProductosApplication.class, args);
     }
-
-} 
+}
