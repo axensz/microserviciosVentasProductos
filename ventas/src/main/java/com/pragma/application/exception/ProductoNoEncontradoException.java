9c3f6c27e94cab898d0a0bc5d33599e515f3ebc8
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2025-05-19 17:54:38 -0500

    refactor: elimina microservicio ventas
    Se elimina el microservicio de ventas por errores críticos. Se mejorará la estructura y clases del proyecto.

--- ventas/src/main/java/com/pragma/application/exception/ProductoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.pragma.application.exception;
-
-public class ProductoNoEncontradoException extends RuntimeException {
-    public ProductoNoEncontradoException(String message) {
-        super(message);
-    }
-}
