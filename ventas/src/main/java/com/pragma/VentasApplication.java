2fd87f55a7b92a8eeb3e281cabce81065b4905b0
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2025-05-19 09:44:04 -0500

    Estructura y configurar POM para microservicio de ventas
    - Estructura con base a arquitectura limpia del microservicio de ventas.
    - Actualizado pom.xml de ventas con dependencias necesarias: Spring Cloud OpenFeign, MapStruct, Lombok, Springdoc OpenAPI y Jacoco.`n- Habilitado FeignClients en la clase VentasApplication.

--- ventas/src/main/java/com/pragma/VentasApplication.java
+++ ventas/src/main/java/com/pragma/VentasApplication.java
@@ -1,13 +1,15 @@
-package com.pragma.ventas;
+package com.pragma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class VentasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VentasApplication.class, args);
     }
 
-} 
+}
