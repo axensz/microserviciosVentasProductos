b17648e8e6685b8d4468e16955de8fbdd0d134f9
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2025-05-19 13:30:23 -0500

    Arreglado: Configuracion en rest  mapper

--- ventas/src/main/java/com/pragma/infrastructure/input/rest/mapper/VentaRestMapper.java
+++ ventas/src/main/java/com/pragma/infrastructure/input/rest/mapper/VentaRestMapper.java
@@ -5,15 +5,25 @@ import com.pragma.application.dto.VentaResponseDto;
 import com.pragma.application.mapper.VentaDtoMapper;
 import com.pragma.domain.model.Venta;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {VentaDtoMapper.class})
 public interface VentaRestMapper {
 
+    // Map from request DTO to domain model
     Venta toVenta(VentaRequestDto ventaRequestDto);
-
-    VentaResponseDto toVentaResponseDto(Venta venta);
-
-    List<VentaResponseDto> toVentaResponseDtoList(List<Venta> ventas);
+    
+    // Delegate domain-to-dto mapping to VentaDtoMapper
+    default VentaResponseDto toVentaResponseDto(Venta venta) {
+        return getVentaDtoMapper().toResponse(venta);
+    }
+    
+    default List<VentaResponseDto> toVentaResponseDtoList(List<Venta> ventas) {
+        return getVentaDtoMapper().toResponseList(ventas);
+    }
+    
+    // Abstract method to obtain VentaDtoMapper (will be implemented by MapStruct)
+    VentaDtoMapper getVentaDtoMapper();
 }
