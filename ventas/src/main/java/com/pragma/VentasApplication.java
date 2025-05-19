30727bcdf5075e9f9304467568f23a099b56778e
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2025-05-19 10:47:57 -0500

    Agregada: Configuración inicial del proyecto, .gitignore, pom.xml, clase principal y propiedades

--- ventas/src/main/java/com/pragma/VentasApplication.java
+++ ventas/src/main/java/com/pragma/VentasApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Clase principal de la aplicación de Ventas.
+ * Habilita los clientes Feign para la comunicación con otros microservicios.
+ */
 @SpringBootApplication
 @EnableFeignClients
 public class VentasApplication {
