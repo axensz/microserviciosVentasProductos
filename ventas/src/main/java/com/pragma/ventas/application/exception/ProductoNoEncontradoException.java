c9e1fa7dc6bba421215ee461dca528dba821bb74
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2025-05-19 23:00:21 -0500

    Eliminado: Referencias obsoletas - Eliminación de archivos antiguos - Limpieza de imports no utilizados

--- ventas/src/main/java/com/pragma/ventas/application/exception/ProductoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.pragma.ventas.application.exception;
-
-public class ProductoNoEncontradoException extends RuntimeException {
-    public ProductoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-} 
--- ventas/src/main/java/com/pragma/ventas/application/port/IVentaPort.java
+++
@@ -1,7 +0,0 @@
-package com.pragma.ventas.application.port;
-
-import com.pragma.ventas.domain.model.Venta;
-
-public interface IVentaPort {
-    Venta registrarVenta(Venta venta);
-} 
