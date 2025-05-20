6907f7e02b8f50c0f657a1b4ec83d1320f17b3a8
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2025-05-19 22:16:17 -0500

    Agregado: Implementación de infraestructura - Cliente Feign y adaptador de gateway

---
+++ ventas/src/main/java/com/pragma/ventas/infrastructure/output/client/ProductoClient.java
@@ -0,0 +1,17 @@
+package com.pragma.ventas.infrastructure.output.client;
+
+import com.pragma.ventas.domain.model.Producto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "productos", url = "${productos.service.url}")
+public interface ProductoClient {
+    
+    @GetMapping("/productos/{id}")
+    ResponseEntity<Producto> obtenerProducto(@PathVariable("id") Long id);
+    
+    @GetMapping("/productos/{id}/existe")
+    ResponseEntity<Boolean> validarExistenciaProducto(@PathVariable("id") Long id);
+} 
