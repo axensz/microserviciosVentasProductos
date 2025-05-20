b3a39c5bf3910d2b39241663c3b368f91e64f1d1
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2025-05-20 08:58:49 -0500

    test: Agregada configuración de seguridad para tests

--- ventas/src/test/java/com/pragma/ventas/infrastructure/input/controller/VentaControllerTest.java
+++ ventas/src/test/java/com/pragma/ventas/infrastructure/input/controller/VentaControllerTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,6 +23,7 @@ import java.util.List;
 
 @WebMvcTest(VentaController.class)
 @ActiveProfiles("test")
+@WithMockUser(roles = "VENDEDOR")
 class VentaControllerTest {
     @Autowired
     private MockMvc mockMvc;
